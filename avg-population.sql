a9b8120cca4ffc6b130ccbfc93a8f26f79beb639
Shanvi <sanvi6398@gmail.com>
2026-02-27 23:14:13 +0530

    Create avg-population.sql

---
+++ avg-population.sql
@@ -0,0 +1,7 @@
+--Hackerrank - https://www.hackerrank.com/challenges/average-population-of-each-continent/problem?isFullScreen=true
+-- Solution 
+SELECT COUNTRY.Continent, FLOOR(AVG(CITY.Population))
+FROM CITY
+JOIN COUNTRY
+ON CITY.CountryCode = COUNTRY.Code
+GROUP BY COUNTRY.Continent;
